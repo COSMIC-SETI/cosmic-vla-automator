54b825726879deb6e4c12002781b3092f64c72ca
danielczech <daniel.czech@protonmail.com>
2022-10-25 12:11:05 +0000

    F-engine state retrieval.

--- automator/interface.py
+++ automator/interface.py
@@ -5,6 +5,9 @@ import sys
 from logger import log
 import utils
 
+from cosmic.fengines import ant_remotefeng_map
+
+
 class Interface(object):
     """Observing interface class. Provides functions to execute
     some basic observing actions. 
@@ -23,10 +26,28 @@ class Interface(object):
     def __init__(self):
         self.r = redis.StrictRedis(decode_responses=True)
 
-    def fengine_state(self):
-        """Determines the current state of the F-engines.
+    def fengine_state(self, stragglers=0):
+        """Determines the (aggregate) current state of the F-engines.
         """
+        # Retrieve F-engine to antenna mapping:
 
+        feng_antenna_map = ant_remotefeng_map.get_antennaFengineDict(self.r)
+        # Check F-engine states:
+        n_fengines = 0
+        n_enabled = 0
+        for antenna, fengine in feng_antenna_map.items():
+            tx_status = fengine.tx_enabled()
+            # tx_enabled() returns [1] if transmitting
+            if(tx_status[0] == 1):
+                n_fengines += 1
+                n_enabled += 1
+            else:
+                n_fengines += 1
+                log.warning('F-engine for antenna: {} is not enabled'.format(antenna))
+        if(n_enabled >= n_fengines - stragglers):
+            return 'enabled'
+        else:
+            return 'disabled'
 
     def expected_antennas(self, meta_hash='META', antenna_key='station'):
         """Retrieve the list of antennas that are expected to be used 
@@ -88,8 +109,10 @@ def cli(args = sys.argv[0]):
     interface = Interface()
 
     if len(sys.argv) < 2:
-        print("\nSelect a command from the following:\n")
-        print("telescope_state    current state of the telescope")
+        print("\nSelect a command from the following:")
+        print("\n    telescope_state      current state of the telescope")
+        print("\n    fengine_state        aggregate F-engine state")
+        print("\n    expected_antennas    list of antennas which should be active")
         return
     
     command = sys.argv[1]
@@ -97,6 +120,12 @@ def cli(args = sys.argv[0]):
 
     if command == 'telescope_state':
         print(interface.telescope_state())
+        return
+    if command == 'fengine_state':
+        print(interface.fengine_state())
+        return
+    if command == 'expected_antennas':
+        print(interface.expected_antennas())
         return
 
     else:
