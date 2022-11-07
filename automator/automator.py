b81b0be1f79fff6d24103ea20e998a182c834dd9
danielczech <daniel.czech@protonmail.com>
2022-11-07 00:14:07 +0000

    Core automator.

--- automator/automator.py
+++ automator/automator.py
@@ -1,6 +1,8 @@
 import redis
+
 from interface import Interface
 from logger import log
+from utils import Utils
 
 class Automator(object):
     """Automation for commensal observing with the COSMIC system at the VLA.
@@ -22,7 +24,6 @@ class Automator(object):
 
     TODO: implement retries for certain operations
     TODO: implement slack notifications for operational stages
-
     """
 
     def __init__(self, redis_endpoint, antenna_key, instances, daq_domain, 
@@ -36,70 +37,149 @@ class Automator(object):
         Returns:
             None
         """
-        log.info('Starting Automator:\n'
-                 'Redis endpoint: {}\n'.format(redis_endpoint))
         redis_host, redis_port = redis_endpoint.split(':')
+        # Redis connection:
         self.r = redis.StrictRedis(host=redis_host, 
                                               port=redis_port, 
                                               decode_responses=True)
+        # Utilities:
+        self.u = Utils()
+        # Interface:
+        self.interface = Interface()
         self.antenna_hash_key = antenna_key
         self.instances = instances
         self.daq_domain = daq_domain
         self.duration = duration
+
 
     def start(self):
         """Start the automator. Actions to be taken depend on the incoming 
         observational stage messages on the appropriate Redis channel. 
         """   
         
-        utils.alert('Starting up...')
-
+        self.u.alert('Starting up...')
         ps = self.r.pubsub(ignore_subscribe_messages=True)
-        
-        # Check if we are already on source:
-        tel_state_on_startup = Interface.telescope_state(
-            antenna_hash=self.antenna_hash_key)
-        if tel_state_on_startup == 'on_source':
-            # If we are on source, potentially initiate recording for any 
-            # available processing nodes 
-            rec_instances = Interface.record_conditional(self.daq_domain, 
-                                         self.instances, 
-                                         self.duration)                 
-            # Listen to each hashpipe instance hash to monitor recording:
+        telescope_on_startup = self.interface.telescope_state(
+            antenna_hash=self.antenna_hash_key
+            )
+        if telescope_on_startup == 'on_source':
+            self.telescope_on_source(self, ps)
 
-        else:
-            utils.alert('Telescope in state: {}'.format(tel_state_on_startup))
-            rec_instances = []
-
-        # Listen to antenna station key and compare allocated antennas with 
+        # Listen to antenna station key to compare allocated antennas with 
         # on-source antennas to determine recording readiness 
         ps.subscribe('__keyspace@0__:{}').format(self.antenna_hash_key)
+
+        # Check incoming messages. 
         for updated_key in ps.listen():
-            if updated_key['data'] == 'set':
-                # Check what was updated:
-                channel = updated_key['channel'].split(':')[1]
+            if updated_key['data'] == 'hset':
+                # Check which hash was updated (note, we can only detect if
+                # entire hash was updated)
+                channel = updated_key['channel'].split(':', 1)[1]
+                
                 # If the antenna flags have been updated, check if the telescope 
                 # has transitioned between off_source and on source:
                 if channel == self.antenna_hash_key:
-                    tel_state = Interface.telescope_state(antenna_hash=self.antenna_hash_key)
-                    # Stop recording for all instances if telescope moves off 
-                    # source during recording:
-                    if tel_state == 'off_source':
-                        daq_states = self.daq_states(daq_domain, instances)
-                        if len(daq_states['recording']) > 0:
-                            Interface.stop_recording()
-                            # Unsubscribe from any recording keyspace notifications:
+                    self.telescope_state_change(ps)
 
-                            # Potentially start processing here:
+                # If this is a recording update:
+                else:
+                    self.recording_state_change(ps, channel)
 
-                    # Potentially start recording if the telescope moves on source:
-                    elif tel_state == 'on_source':
-                        rec_instances = Interface.record_conditional(self.daq_domain, 
-                                                     self.instances, 
-                                                     self.duration)                 
-                        # Listen to each hashpipe instance hash to monitor recording:
 
-            # If we stop processing, check if we should start postprocessing
+    def recording_state_change(self, ps, channel):
+        """Actions to take if recording state changes.
+        """
+        instance = self.parse_instance(channel)
+        if instance is not None:
+            # Get new recording state:
+            new_state = self.u.daq_record_state(self.daq_domain, instance)
+            if new_state == 'idle':
+                # Transition to idle
+                # Unsubscribe from any recording keyspace notifications
+                self.unsubscribe_instances(self.daq_domain, [instance], ps)
 
-            # If we stop postprocessing, check if we should start recording
+                self.u.alert('Would initiate processing: {}'.format(instance))
+            else:
+                self.u.alert('{}: new state: {}'.format(instance, new_state))
+
+
+    def parse_instance(self, channel):
+        """Given a keyspace channel name for a possible instance, retrieve
+        the potential instance name and check if it is on the list of 
+        available instances. 
+        """
+        try:
+            instance = channel.split('{}://', 1)[1]
+            instance = instance.split('/status')[0]
+            if instance in self.instances:
+                return instance
+            self.u.alert('Instance not in local instance list')
+            return 
+        except:
+            self.u.alert('Could not parse pubsub message: {}'.format(channel))
+            return
+
+
+    def telescope_state_change(self, ps):
+        """Actions to take if telescope state changes.
+        """
+        # Retrieve new telescope state:
+        tel_state = self.interface.telescope_state(
+            antenna_hash=self.antenna_hash_key
+            )
+        # Stop recording for all instances if telescope moves off 
+        # source during recording:
+        if tel_state == 'off_source':
+            self.telescope_off_source(ps)
+        # Potentially start recording if the telescope moves on source:
+        elif tel_state == 'on_source':
+            self.telescope_on_source(ps)
+
+
+    def telescope_off_source(self, ps):
+        """If the telescope moves off source, the following actions are taken.
+        """
+        daq_states = self.interface.daq_states(self.daq_domain, self.instances)
+        recording = daq_states['recording']
+        if len(recording)) > 0:
+            self.interface.stop_recording()
+            # Unsubscribe from any recording keyspace notifications
+            self.unsubscribe_instances(self.daq_domain, recording, ps)
+            # Potentially start processing here:
+            self.u.alert('Processing would begin for: {}'.format(recording))
+
+
+    def telescope_on_source(self, ps):
+        """If the telescope moves on source, the following actions
+        are taken. 
+        """
+        # If we are on source, potentially initiate recording for any 
+        # available processing nodes 
+        rec_instances = self.interface.record_conditional(
+            self.daq_domain,
+            self.instances,
+            self.duration
+            )
+        if len(rec_instances) > 0:
+            # Subscribe to hashpipe instance hashes to monitor recording:
+            self.subscribe_instances(self.daq_domain, rec_instances, ps)
+
+
+    def subscribe_instances(self, domain, instances, ps):
+        """Subscribe to monitor each instance's gateway hash.
+        """
+        for instance in instances:
+           instance_channel = '{}://{}/status'.format(domain, instance)
+           ps.subscribe('__keyspace@0__:{}'.format(instance_channel))
+
+
+    def unsubscribe_instances(self, domain, instances, ps):
+        """Unsubscribe from an instance's gateway hash.
+        """
+        for instance in instances:
+            instance_channel = '{}://{}/status'.format(domain, instance)
+            ps.unsubscribe('__keyspace@0__:{}'.format(instance_channel))
+
+
+
 
