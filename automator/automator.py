ea4f3f6e795a6e04cbb361da774a104a33e55caf
danielczech <daniel.czech@protonmail.com>
2023-01-17 15:40:37 +0000

    VLASS recording triggered via ScanIntent - antennna flagging will go in the block headers.

--- automator/automator.py
+++ automator/automator.py
@@ -39,8 +39,10 @@ class Automator(object):
             redis_host,
             redis_port
         )
-        self.antenna_hash_key = redis_channel
-        self.telescope_state = 'unknown'
+        self.redis_channel = redis_channel
+        self.vlass_state = 'unknown'
+        self.recording_state = 'unknown'
+        self.processing_state = 'unknown'
 
     def start(self):
         """Start the automator. Actions to be taken depend on the incoming 
@@ -48,69 +50,59 @@ class Automator(object):
         """   
         
         self.u.alert('Starting up...')
+
         ps = self.r.pubsub(ignore_subscribe_messages=True)
-        self.u.alert('Listening to telescope state...')
-        ps.subscribe('__keyspace@0__:{}'.format(self.antenna_hash_key))
+        self.u.alert('Waiting for a VLASS track to start...')
+        ps.subscribe(self.redis_channel)
 
         for msg in ps.listen():
 
-            # For now, monitor antennas to determine if the 
-            # telescope is on source or not. 
-            if msg['data'] == 'hset':
-                new_telescope_state = self.interface.telescope_state()
+            # Awaiting an active VLASS track:
+            if msg['data'] == 'vlass-track':
                 if new_telescope_state != self.telescope_state:
                     self.telescope_state = new_telescope_state
                     self.telescope_state_change(new_telescope_state)
 
-    def telescope_state_change(self, new_state):
+    def vlass_state_change(self, new_state):
         """Actions to take if the state of the telescope changes.
         """
         
-        if new_state == 'off_source':
+        # If transitioning out of vlass track:
+        if not new_state and self.vlass_state:
             # Stop recording
             self.interface.stop_all()
+            self.vlass_state = new_state
         
-        if new_state == 'on_source':    
-
-            # Check if VLASS calibrator:
-            if self.interface.is_vlass_cal():
-                # Instruct recording to start
-                # How long do we want on calibrators?
-                self.interface.record_minimal(time.time() + 1, 60, 'COSMIC_TEST')
-                self.u.alert('Recording VLASS calibrator.')
-
-            # Check if VLASS track:
-            if self.interface.is_vlass_track():
-                # Retrieve metadata:
-                ra, dec, fcent, ra_rate, ts = self.interface.vlass_metadata()
-                # Calculate phase center:
-                # Using VLASS standard slew rate of 3.3 arcmin/sec (0.055 deg/sec) 
-                # until ra_rate units are understood
-                ra_c, dec_c = self.select_phase_center(0.055, ts, ra, dec)
-                self.r.set('phase_center_ra', '{ra_c}')
-                self.r.set('phase_center_dec', '{dec_c}')
-                # Request new targets around phase center
-                self.interface.request_targets(
-                    TARGETS_CHAN, 
-                    ts, 
-                    'VLASS', 
-                    ra_c, 
-                    dec_c, 
-                    fcent
-                    )
-                # Instruct recording to start
-                self.interface.record_minimal(time.time() + 1, 10, 'COSMIC_TEST')
-                self.u.alert('Recording VLASS track.')
+        # If transitioning into vlass track:
+        if new_state and not self.vlass_state:    
+            self.vlass_state = new_state
+            # Retrieve metadata:
+            ra, dec, fcent, ra_rate, ts = self.interface.vlass_metadata()
+            log.info(self.interface.vlass_metadata())
+            # Calculate phase center:
+            # Using VLASS standard slew rate of 3.3 arcmin/sec (0.055 deg/sec) 
+            # until ra_rate units are understood
+            ra_c, dec_c = self.select_phase_center(0.055, ts, ra, dec)
+            self.r.set('phase_center_ra', f'{ra_c}')
+            self.r.set('phase_center_dec', f'{dec_c}')
+            # Request new targets around phase center
+            self.interface.request_targets(
+                TARGETS_CHAN, 
+                ts, 
+                'VLASS', 
+                ra_c, 
+                dec_c, 
+                fcent
+                )
+            # Instruct recording to start
+            self.interface.record_minimal(time.time() + 1, 10, 'COSMIC_TEST_a')
+            self.u.alert('Recording VLASS track.')
             
-            else:
-                self.u.alert('Not a VLASS track or a VLASS calibrator.')
-                
-
 
     def offset_ra(self, angle, ra, dec):
         """Return new RA given a separation.
         """
-        start = SkyCoord(ra, dec, units="deg")
+        start = SkyCoord(ra, dec, unit="deg")
         offset = start.directional_offset_by(90*u.deg, angle*u.deg)
         return offset.ra.degree, offset.dec.degree
 
@@ -123,7 +115,7 @@ class Automator(object):
         # Separation since packet received (5 seconds to phase center)
         # Add another buffer of 1 second (will specify tstart 1 sec in 
         # the future)
-        ra_sep_start = (self.mjd_now() - t_start + 6)*slew_rate
+        ra_sep_start = (self.mjd_now() - float(t_start) + 6)*float(slew_rate)
         ra, dec = self.offset_ra(ra_sep_start, ra, dec)
         return ra, dec
 
