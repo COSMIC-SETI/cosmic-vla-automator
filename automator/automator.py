036aa8fb86e7856779f64b5cf44f5ffdca615ac5
daniel <daniel.czech@protonmail.com>
2023-01-25 23:54:07 +0200

    Integrating recording and processing states into automator.

--- automator/automator.py
+++ automator/automator.py
@@ -41,8 +41,8 @@ class Automator(object):
         )
         self.redis_channel = redis_channel
         self.vlass_state = 'unknown'
-        self.recording_state = 'unknown'
-        self.processing_state = 'unknown'
+        self.rec_state = 'unknown'
+        self.proc_state = 'unknown'
 
     def start(self):
         """Start the automator. Actions to be taken depend on the incoming 
@@ -51,53 +51,110 @@ class Automator(object):
         
         self.u.alert('Starting up...')
 
-        ps = self.r.pubsub(ignore_subscribe_messages=True)
-        self.u.alert('Waiting for a VLASS track to start...')
-        ps.subscribe(self.redis_channel)
+        # Check current states:
+        # Are we processing?
+        self.proc_update()
+
+        # Are we recording?
+        self.rec_update()
+
+        # Is VLASS currently tracking?
+        if self.interface.is_vlass_track():
+            self.vlass_state = True
+            self.vlass_state_change(True)
+
+        self.ps = self.r.pubsub(ignore_subscribe_messages=True)
+        self.u.alert('Listening for VLASS, processing and recording updates.')
+        self.ps.subscribe(self.redis_channel)
 
         for msg in ps.listen():
 
             # Awaiting an active VLASS track:
             if msg['data'] == 'vlass-track':
                 if new_telescope_state != self.telescope_state:
-                    self.telescope_state = new_telescope_state
-                    self.telescope_state_change(new_telescope_state)
+                    self.vlass_state = new_telescope_state
+                    self.vlass_state_change(new_telescope_state)
+
+            # Check for recording updates:
+            if msg['data'] == 'rec_update':
+                self.rec_update()
+
+            # Check for processing updates:
+            if msg['data'] == 'proc_update':
+                self.proc_update()
+
+    def proc_state_change(self, new_state):
+        """Actions to take if the processing state changes
+        """
+        if not new_state and self.proc_state:
+            # Processing is finished. 
+            self.proc_state = False 
+            # Check if we should record a new vlass segment:
+            if self.vlass_state:
+                self.record_track()     
+            else:
+                self.u.alert('Processing complete, but VLASS is no longer tracking.')
+                self.u.alert('Waiting for a new VLASS track.')
+
+    def proc_update(self):
+        """Checks current processing state. 
+        """
+
+    def rec_update(self):
+        """Checks current recording state.
+        """
+
+    def rec_state_change(self, new_state):
+        """Actions to take if the recording state changes:
+        """ 
+        self.rec_state = new_state 
 
     def vlass_state_change(self, new_state):
         """Actions to take if the state of the telescope changes.
         """
-        
-        # If transitioning out of vlass track:
+
+        # If transitioning out of vlass track, we need to stop recording
+        # immediately:
         if not new_state and self.vlass_state:
             # Stop recording
             self.interface.stop_all()
             self.vlass_state = new_state
         
-        # If transitioning into vlass track:
-        if new_state and not self.vlass_state:    
+        # If we are already recording or processing, do not record a new track
+        elif self.rec_state:
+            self.u.alert('Already recording segment for current track.')
+        elif self.proc_state:
+            self.u.alert('Waiting for processing of previous segment to finish.')
+        
+        # If we are not recording or processing, and vlass has started tracking:
+        elif new_state:    
             self.vlass_state = new_state
-            # Retrieve metadata:
-            ra, dec, fcent, ra_rate, ts = self.interface.vlass_metadata()
-            log.info(self.interface.vlass_metadata())
-            # Calculate phase center:
-            # Using VLASS standard slew rate of 3.3 arcmin/sec (0.055 deg/sec) 
-            # until ra_rate units are understood
-            ra_c, dec_c = self.select_phase_center(0.055, ts, ra, dec)
-            self.r.set('phase_center_ra', f'{ra_c}')
-            self.r.set('phase_center_dec', f'{dec_c}')
-            # Request new targets around phase center
-            self.interface.request_targets(
-                TARGETS_CHAN, 
-                ts, 
-                'VLASS', 
-                ra_c, 
-                dec_c, 
-                fcent
-                )
-            # Instruct recording to start
-            self.interface.record_minimal(time.time() + 1, 10, 'COSMIC_TEST_a')
-            self.u.alert('Recording VLASS track.')
-            
+            self.record_track()     
+
+    def record_track(self):
+        """Record a VLASS track!
+        """
+        # Retrieve metadata:
+        ra, dec, fcent, ra_rate, ts = self.interface.vlass_metadata()
+        log.info(self.interface.vlass_metadata())
+        # Calculate phase center:
+        # Using VLASS standard slew rate of 3.3 arcmin/sec (0.055 deg/sec) 
+        # until ra_rate units are understood
+        ra_c, dec_c = self.select_phase_center(0.055, ts, ra, dec)
+        self.r.set('phase_center_ra', f'{ra_c}')
+        self.r.set('phase_center_dec', f'{dec_c}')
+        # Request new targets around phase center
+        self.interface.request_targets(
+            TARGETS_CHAN, 
+            ts, 
+            'VLASS', 
+            ra_c, 
+            dec_c, 
+            fcent
+            )
+        # Instruct recording to start
+        self.interface.record_minimal(time.time() + 1, 10, 'COSMIC_TEST_a')
+        self.u.alert('Recording a new VLASS track.')
 
     def offset_ra(self, angle, ra, dec):
         """Return new RA given a separation.
